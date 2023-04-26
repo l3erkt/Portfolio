2f67138a43053c9f87deabbd1c1bb2ab3ace707c
bereket zeggai <bereketzeggai52@gmail.com>
2023-04-26 19:16:31 -0400

    dynamic mobile nav -- text detail change

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("hello")
+/* Mobile Nav-section */
+const navLinks = document.querySelector(".nav-links");
+const closeBtn = document.getElementById("close-nav-btn");
+const hamburger = document.getElementById("hamburger");
+
+
+hamburger.addEventListener('click', () => {
+    navLinks.style.transform = "translateY(0)"
+})
+
+closeBtn.addEventListener('click', () => {
+    navLinks.style.transform = ""
+})
