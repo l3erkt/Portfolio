54d1ec6d153bc6fe92c3109dd7d9060a5e646cd3
bereket zeggai <bereketzeggai52@gmail.com>
2023-04-26 19:52:35 -0400

    fixed boxshadow nav

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 /* Mobile Nav-section */
 const navLinks = document.querySelector(".nav-links");
+const dimBkg = document.querySelector(".bkg-dim");
 const closeBtn = document.getElementById("close-nav-btn");
 const hamburger = document.getElementById("hamburger");
 
 
-hamburger.addEventListener('click', () => {
-    navLinks.style.transform = "translateY(0)"
-})
+hamburger.addEventListener("click", () => {
+  navLinks.style.transform = "translateY(0)";
+});
 
-closeBtn.addEventListener('click', () => {
-    navLinks.style.transform = ""
-})
+closeBtn.addEventListener("click", () => {
+  navLinks.style.transform = "";
+});
